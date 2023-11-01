148e46c78ad0347a134d97639420f83ad746ad6a
vipulprakashrai <103874813+vipulprakashrai@users.noreply.github.com>
2023-11-01 16:07:14 +0530

    ep-03 part2, React Component, Functional Component, Component Composition

--- App.js
+++ App.js
@@ -1,22 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-// React.createElement => ReactElement(Object) => HTMLElement(on render)
-const heading = React.createElement("h1",{id: "heading"},"Learning React")
-console.log(heading)//Object
-
-// JSX-(HTML like or XML like Syntax)
-// jsx -> transpiled before it reaches the js engine -> done by babel  -> PARCEL
-
-//JSX =>Babel transpiles it to React.createElement => ReactElement(Object) => HTMLElement(on render)
-const jsxHeading = (
+// React Element
+const heading = (
     <h1 className="heading" tabIndex={1}>
      React using JSX
     </h1>
-  )
-console.log(jsxHeading);
+  );
+ /*
+ React Component
+ 1) Class Based Component (old)
+2) Functional Component (new)
+*/
 
+// React Functional Component
+const HeadingComponent = () => (
+  <div>
+    <HeadingComponent2/>
+    <h1 className="heading">This is Functional Component</h1>
+    <HeadingComponent2></HeadingComponent2>
+ </div>
+)
+
+const HeadingComponent2 = () => {
+      return (
+        <>
+          {heading}
+          <h1 className="heading">This is also Functional Component</h1>
+       </>
+      )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading)
-root.render(jsxHeading)
+root.render(<HeadingComponent/>);
