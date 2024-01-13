be39e5988ef7c14ab6cf0c9660a69e067528e703
vipul prakash rai <vipulprakash.rai@gmail.com>
2024-01-13 21:56:05 +0530

    ep-13 testing app

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,16 @@
 export default function Contact(){
     return(
         <>
-        <h1> contact me </h1>
-        
+        <h1> contact us </h1>
+        <div className=" w-52 h-44 flex bg-gray-700 m-10">
+        <form>
+            <input className="border border-black p-1 m-2" type="text" placeholder="name"/>
+            <input className="border border-black p-1 m-2" type="text" placeholder="message"/>
+            <button className="bg-slate-400 rounded p-2 m-2 ml-14">Submit</button>
+        </form>
+
+        </div>
+      
         </>
     )
 }
--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -9,3 +9,33 @@ test("is my heading is shown", ()=>{
     //Assertion
     expect(heading).toBeInTheDocument(); 
 });
+
+test("is  button is shown in contact component", ()=>{
+    render(
+        <Contact/>
+    );
+    //const button = screen.getByRole("button");
+    const button = screen.getByText("Submit");
+    //Assertion
+    expect(button).toBeInTheDocument() 
+});
+
+test("is  input is rendered in the contact component", ()=>{
+    render(
+        <Contact/>
+    );
+
+    const input = screen.getAllByRole("textbox");
+    //Assertion
+    expect(input.length).toBe(2); 
+});
+
+test("is  input name is shown in contact component", ()=>{
+    render(
+        <Contact/>
+    );
+    
+    const inputName = screen.getByPlaceholderText("name");
+    //Assertion
+    expect(inputName).toBeInTheDocument(); 
+});
